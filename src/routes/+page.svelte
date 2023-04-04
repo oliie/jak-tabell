b93abc4180b97a9f5ca16b981438548912f731d3
Oliver Praesto <oliver.praesto@gmail.com>
2023-04-04 23:32:41 +0200

    - adds phatt features 🚀

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,46 @@
 <script lang="ts">
-	let headings = ['M3 Tabellnamn', 'M3 Kolumnnamn', 'Koppling till annan M3 Tabell', 'Mätetal'];
+	import { onMount } from 'svelte';
 
-	let data = [
-		['a', 'b', 'c', 'd'],
-		['e', 'f', 'g', 'h'],
-		['i', 'j', 'k', 'l']
+	const INITIAL_DATA = [['', '', '', '']];
+	const INITIAL_HEADINGS = [
+		'M3 Tabellnamn',
+		'M3 Kolumnnamn',
+		'Koppling till annan M3 Tabell',
+		'Mätetal'
 	];
+
+	let headings = structuredClone(INITIAL_HEADINGS);
+	let data: string[][] = structuredClone(INITIAL_DATA);
+
+	onMount(() => {
+		const storedHeadings = localStorage.getItem('HEADINGS');
+		const storedData = localStorage.getItem('DATA');
+
+		if (!!storedHeadings) headings = JSON.parse(storedHeadings);
+		if (!!storedData) data = JSON.parse(storedData);
+	});
+
+	$: isValid = data.every((row) =>
+		row.every((_val, _i, arr) => {
+			return !!arr[0] && !!arr[1];
+		})
+	);
+
+	const storeData = () => {
+		localStorage.setItem('HEADINGS', JSON.stringify(headings));
+		localStorage.setItem('DATA', JSON.stringify(data));
+	};
 
 	const addRow = (index: number) => {
 		data.splice(index + 1, 0, new Array(data[0].length).fill(null));
 		data = data;
+		storeData();
 	};
 
 	const removeRow = (index: number) => {
 		data.splice(index, 1);
 		data = data;
+		storeData();
 	};
 
 	const addColumn = () => {
@@ -23,6 +49,13 @@
 
 		headings = headings;
 		data = data;
+		storeData();
+	};
+
+	const resetTable = () => {
+		headings = structuredClone(INITIAL_HEADINGS);
+		data = structuredClone(INITIAL_DATA);
+		storeData();
 	};
 
 	const exportToExcel = () => {
@@ -52,7 +85,12 @@
 						<th>{heading}</th>
 					{:else}
 						<th>
-							<input placeholder="empty" class="bg-transparent p-2 w-full" bind:value={heading} />
+							<input
+								placeholder="empty"
+								class="bg-transparent p-2 w-full"
+								on:keyup={storeData}
+								bind:value={heading}
+							/>
 						</th>
 					{/if}
 				{/each}
@@ -66,7 +104,12 @@
 				<tr class="m-0 p-0">
 					{#each row as value}
 						<td class="m-0 p-0">
-							<input placeholder="empty" class="bg-transparent p-2 w-full" bind:value />
+							<input
+								placeholder="empty"
+								class="bg-transparent p-2 w-full"
+								on:keyup={storeData}
+								bind:value
+							/>
 						</td>
 					{/each}
 					<td class="m-0 p-0">
@@ -81,6 +124,11 @@
 	</table>
 </div>
 
-<div class="mt-8 ml-2">
-	<button class="btn btn-secondary" on:click={exportToExcel}>Export to Excel</button>
+<div class="text-center">
+	<div class="mt-8 ml-2 grid grid-cols-2 gap-4 w-1/4">
+		<button class="btn btn-primary" on:click={resetTable}> Reset </button>
+		<button disabled={!isValid} class="btn btn-secondary" on:click={exportToExcel}>
+			Export to Excel
+		</button>
+	</div>
 </div>
